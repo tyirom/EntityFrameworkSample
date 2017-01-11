e19a83eb4a68ccc44762a1ad3b0fd71e1458d1f4
moriyamat <moriyamat@DEV-MORIYAMA-03>
2017-01-11 18:21:42 +0900

    Multi-threading for EF6 and SQLite.

--- EntityFrameworkSample/MediaSample.cs
+++ EntityFrameworkSample/MediaSample.cs
@@ -3,6 +3,12 @@
 //   Copyright (C) 2017 Grass Valley K.K. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Data.Entity;
+using System.Threading;
+
 namespace EntityFrameworkSample
 {
 	using System;
@@ -43,7 +49,7 @@ namespace EntityFrameworkSample
 			{
 				foreach (var media in db.Medias.Select(i => i))
 				{
-					Console.WriteLine("media path:{0} create:{1} update:{2}", media.FilePath, media.CreationTime, media.LastWriteTime);
+//					Console.WriteLine("media path:{0} create:{1} update:{2}", media.FilePath, media.CreationTime, media.LastWriteTime);
 				}
 			}
 		}
@@ -52,6 +58,129 @@ namespace EntityFrameworkSample
 		{
 			using (var db = new Repository())
 			{
+//				db.Database.Log = log => System.Diagnostics.Trace.Write(log);
+//				db.Configuration.ProxyCreationEnabled = false;
+				return db.Medias.Any(m => m.FilePath == filePath);
+			}
+		}
+	}
+
+	public class MediaSample2
+	{
+		public readonly Repository _db;
+
+		public MediaSample2()
+		{
+			this._db = new Repository();
+//			this._db.Database.Log = log => System.Diagnostics.Trace.Write(log);
+//			this._db.Configuration.ProxyCreationEnabled = false;
+		}
+
+		~MediaSample2()
+		{
+			this._db.Dispose();
+		}
+
+		public void Add(FileInfo fi)
+		{
+			var media = new Media();
+			media.FilePath = fi.FullName;
+			media.CreationTime = fi.CreationTime;
+			media.LastWriteTime = fi.LastWriteTime;
+			this._db.Medias.Add(media);
+			this._db.SaveChanges();
+		}
+
+		public void Dump()
+		{
+			foreach (var media in this._db.Medias.Select(i => i))
+			{
+//				Console.WriteLine("media path:{0} create:{1} update:{2}", media.FilePath, media.CreationTime, media.LastWriteTime);
+			}
+		}
+
+		public bool HasMedia(string filePath)
+		{
+			return this._db.Medias.Any(m => m.FilePath == filePath);
+		}
+	}
+
+	public class MediaSample3
+	{
+//		public readonly Repository _db;
+		public static Repository Db;
+		private static System.Threading.Thread Worker;
+		private static bool IsRunning = true;
+		private static ConcurrentQueue<FileInfo> Queue;
+
+		static MediaSample3()
+		{
+			Queue = new ConcurrentQueue<FileInfo>();
+			Db = new Repository();
+			Worker = new Thread(() =>
+			{
+				while (IsRunning)
+				{
+					if (Queue.IsEmpty)
+					{
+						continue;
+					}
+
+					FileInfo fi;
+					if (Queue.TryDequeue(out fi))
+					{
+						var media = new Media();
+						media.FilePath = fi.FullName;
+						media.CreationTime = fi.CreationTime;
+						media.LastWriteTime = fi.LastWriteTime;
+						Db.Medias.Add(media);
+						Db.SaveChanges();
+					}
+				}
+			});
+		}
+
+		public MediaSample3()
+		{
+//			this._db = new Repository();
+			//			this._db.Database.Log = log => System.Diagnostics.Trace.Write(log);
+			//			this._db.Configuration.ProxyCreationEnabled = false;
+		}
+
+		~MediaSample3()
+		{
+//			this._db.Dispose();
+			Db.Dispose();
+		}
+
+		public void Add(FileInfo fi)
+		{
+//			var media = new Media();
+//			media.FilePath = fi.FullName;
+//			media.CreationTime = fi.CreationTime;
+//			media.LastWriteTime = fi.LastWriteTime;
+//			this._db.Medias.Add(media);
+//			this._db.SaveChanges();
+			Queue.Enqueue(fi);
+		}
+
+		public void Dump()
+		{
+			using (var db = new Repository())
+			{
+				foreach (var media in db.Medias.Select(i => i))
+				{
+					//					Console.WriteLine("media path:{0} create:{1} update:{2}", media.FilePath, media.CreationTime, media.LastWriteTime);
+				}
+			}
+		}
+
+		public bool HasMedia(string filePath)
+		{
+			using (var db = new Repository())
+			{
+				//				db.Database.Log = log => System.Diagnostics.Trace.Write(log);
+				//				db.Configuration.ProxyCreationEnabled = false;
 				return db.Medias.Any(m => m.FilePath == filePath);
 			}
 		}
--- EntityFrameworkSample/Program.cs
+++ EntityFrameworkSample/Program.cs
@@ -5,29 +5,191 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace EntityFrameworkSample
 {
 	using System.IO;
+	using System.Collections.Generic;
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			var sample = new MediaSample();
 			var di = new DirectoryInfo(@"E:\data\SAIPAN");
+			var timer = new Stopwatch();
+			List<long> results = new List<long>();
+
+			// single thread
+			timer.Start();
+			var sample2 = new MediaSample2();
 			foreach (var fileInfo in di.GetFiles())
 			{
-				if (sample.HasMedia(fileInfo.FullName))
+				if (!sample2.HasMedia(fileInfo.FullName))
 				{
-					continue;
+					sample2.Add(fileInfo);
+//				sample.Add(fileInfo.FullName);
 				}
 
-				sample.Add(fileInfo);
-//				sample.Add(fileInfo.FullName);
+				sample2.Dump();
 			}
+			timer.Stop();
+			results.Add(timer.ElapsedMilliseconds);
+			timer.Reset();
 
+			// multi threads
+			timer.Start();
+			var sample = new MediaSample();
+			Parallel.ForEach(di.GetFiles("*.*", SearchOption.AllDirectories), fileInfo =>
+			{
+				if (!sample.HasMedia(fileInfo.FullName))
+				{
+					sample.Add(fileInfo);
+					//				sample.Add(fileInfo.FullName);
+				}
+
+				sample.Dump();
+			});
+			timer.Stop();
+			results.Add(timer.ElapsedMilliseconds);
+			timer.Reset();
+
+			// read/write
+			// single thread
+			timer.Start();
+			sample2 = new MediaSample2();
+			foreach (var fileInfo in di.GetFiles())
+			{
+				if (!sample2.HasMedia(fileInfo.FullName))
+				{
+					sample2.Add(fileInfo);
+					//				sample.Add(fileInfo.FullName);
+				}
+				else
+				{
+					sample2.Add(fileInfo);
+				}
+
+				sample2.Dump();
+			}
+			timer.Stop();
+			results.Add(timer.ElapsedMilliseconds);
+			timer.Reset();
+
+			// multi threads
+			timer.Start();
+			sample = new MediaSample();
+			Parallel.ForEach(di.GetFiles("*.*", SearchOption.AllDirectories), fileInfo =>
+			{
+				if (!sample.HasMedia(fileInfo.FullName))
+				{
+					sample.Add(fileInfo);
+					//				sample.Add(fileInfo.FullName);
+				}
+				else
+				{
+					sample.Add(fileInfo);
+				}
+
+				sample.Dump();
+			});
+			timer.Stop();
+			results.Add(timer.ElapsedMilliseconds);
+			timer.Reset();
+
+			// read/write
+			// multi threads
+			timer.Start();
+			sample = new MediaSample();
+			Parallel.ForEach(di.GetFiles("*.*", SearchOption.AllDirectories), fileInfo =>
+			{
+				if (!sample.HasMedia(fileInfo.FullName))
+				{
+					sample.Add(fileInfo);
+					//				sample.Add(fileInfo.FullName);
+				}
+				else
+				{
+					sample.Add(fileInfo);
+				}
+
+				sample.Dump();
+			});
+			timer.Stop();
+			results.Add(timer.ElapsedMilliseconds);
+			timer.Reset();
+
+			// single thread
+			timer.Start();
+			sample2 = new MediaSample2();
+			foreach (var fileInfo in di.GetFiles())
+			{
+				if (!sample2.HasMedia(fileInfo.FullName))
+				{
+					sample2.Add(fileInfo);
+					//				sample.Add(fileInfo.FullName);
+				}
+				else
+				{
+					sample2.Add(fileInfo);
+				}
+
+				sample2.Dump();
+			}
+			timer.Stop();
+			results.Add(timer.ElapsedMilliseconds);
+			timer.Reset();
+
+			// read(multi threads)/write(single thread)
+			timer.Start();
+			var sample3 = new MediaSample3();
+			Parallel.ForEach(di.GetFiles("*.*", SearchOption.AllDirectories), fileInfo =>
+			{
+				if (!sample3.HasMedia(fileInfo.FullName))
+				{
+					sample3.Add(fileInfo);
+					//				sample.Add(fileInfo.FullName);
+				}
+				else
+				{
+					sample3.Add(fileInfo);
+				}
+
+				sample3.Dump();
+			});
+			timer.Stop();
+			results.Add(timer.ElapsedMilliseconds);
+			timer.Reset();
+			
+			// single thread
+			timer.Start();
+			sample2 = new MediaSample2();
+			foreach (var fileInfo in di.GetFiles())
+			{
+				if (!sample2.HasMedia(fileInfo.FullName))
+				{
+					sample2.Add(fileInfo);
+					//				sample.Add(fileInfo.FullName);
+				}
+				else
+				{
+					sample2.Add(fileInfo);
+				}
+
+				sample2.Dump();
+			}
+			timer.Stop();
+			results.Add(timer.ElapsedMilliseconds);
+			timer.Reset();
+
+			Console.WriteLine("==================================");
 			sample.Dump();
+
+			foreach (var result in results)
+			{
+				Console.WriteLine("result : {0}msec", result);
+			}
 
 			Console.ReadKey();
 		}
